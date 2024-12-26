8e5f78781e777a5d2303dd8e4e6d48a953588d44
Matyanson <matyas.perem@seznam.cz>
2024-12-26 16:57:30 +0100

    feat: added globe

--- src/routes/scene/+page.svelte
+++ src/routes/scene/+page.svelte
@@ -8,7 +8,7 @@
     let scene: THREE.Scene;
     let camera: THREE.Camera;
     let renderer: THREE.WebGLRenderer;
-    let cube: THREE.Mesh;
+    let sphere: THREE.Mesh;
 
     onMount(()=>{
         createScene();
@@ -18,26 +18,30 @@
         scene = new THREE.Scene();
         camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
         renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
+        
 
-        renderer.setSize( window.innerWidth, window.innerHeight );
+        renderer.setSize( window.innerWidth-1, window.innerHeight-1 );
+        renderer.setPixelRatio(window.devicePixelRatio);
         renderer.setAnimationLoop( animate );
 
-        const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-        const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-        cube = new THREE.Mesh( geometry, material );
-        scene.add( cube );
+        const geometry = new THREE.SphereGeometry(0.5, 50, 50);
+        const material = new THREE.MeshBasicMaterial({
+            map: new THREE.TextureLoader().load('earth_day.jpg')
+        });
+        sphere = new THREE.Mesh( geometry, material );
+        scene.add( sphere );
 
-        camera.position.z = 5;
+        camera.position.z = 1;
     }
 
     function animate() {
 
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
+        sphere.rotation.x += 0.01;
+        sphere.rotation.y += 0.01;
 
         renderer.render( scene, camera );
     }
 
 </script>
 
-<canvas bind:this={canvas} />
+<canvas bind:this={canvas}></canvas>
